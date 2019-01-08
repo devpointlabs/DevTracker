0aebe20d2d3d874db3b2b4063476f57c26fd4141
MichaelJDaniel <soldbymichaeld@gmail.com>
2019-01-08 15:39:51 -0700

    add update method to user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,8 +20,21 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+
   def delete
     @user.destroy
+  end
+
+  def update 
+
   end
 
   private

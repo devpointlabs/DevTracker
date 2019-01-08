1dbfaf0461dd30651e0d6fbee4217c4624b67338
MichaelJDaniel <soldbymichaeld@gmail.com>
2019-01-08 10:30:28 -0700

    Create Auth Provider with all functionality

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+
+const AuthContext = React.createContext();
+export const AuthConsumer = AuthContext.Consumer;
+
+class AuthProvider extends React.Component {
+  state = { user: null, };
+
+  handleRegister = (user, history) => {
+    axios.post("/api/auth", user)
+    .then( res => {
+      this.setState({ user: res.data, });
+      history.push("/");
+    })
+    .catch( err => {
+      console.log(err);
+    })
+  }
+
+  handleLogin = (user, history) => {
+    axios.post("/api/auth/sign_in", user)
+    .then( res => {
+      this.setState({ user: res.data.data, });
+      history.push("/")
+    })
+    .catch( err => {
+      console.log(err.response);
+    })
+  }
+
+  handleLogout = (history) => {
+    axios.delete("/api/auth/sign_out")
+    .then( res => {
+      this.setState({ user: null, })
+    })
+
+  }
+
+  render() {
+    return (
+      <AuthContext.Provider value={{
+      ...this.state,
+      authenticated: this.state.user !== null,
+      handleRegister: this.handleRegister,
+      handleLogin: this.handleLogin,
+      handleLogout: this.handleLogout,
+      setUser: (user) => this.setState({ user, }),
+      }}>
+        { this.props.children }
+      </AuthContext.Provider>
+    )
+  }
+}
+
+export default AuthProvider;

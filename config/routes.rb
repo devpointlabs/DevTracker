aa11ee841cdf0714eb60e8e4d108d8a6de7a7b61
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-08 16:42:55 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  namespace :api do
+    resources :users do
+      resources :applications do
+        resources :interviews
+        resources :offers
+      end
+    end
+    resources :companies
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

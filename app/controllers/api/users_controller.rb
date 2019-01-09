0a0470cf2273476c23898f05c9649df3b4600fdb
Kevin Peery <kevin.peery@gmail.com>
2019-01-09 15:27:48 -0700

    add update method to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,19 +2,43 @@ class Api::UsersController < ApplicationController
   before_action :set_user, only: [:show, :delete, :update]
   before_action :authenticate_user!
 
+  def update
+    user = User.find(params[:id])
+    user.name = params[:name] ? params[:name] : user.name
+    user.email = params[:email] ? params[:email] : user.email
+
+    file = params[:file]
+
+    if file
+      begin
+        ext = File.extname(file.tempfile)
+        cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
+        user.image = cloud_image["secure_url"]
+      rescue => e
+        render json: {errors: e}, status: 422
+      end
+    end
+
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}, status: 422
+    end
+  end
+
   def index
     render json: User.all
   end
-  
-  def show 
+
+  def show
     render json: @user
   end
-  
+
   def create
     user = User.new(user_params)
     if user.save
       render json: user
-    else 
+    else
       render json: user.errors, status: 422
     end
   end
@@ -27,13 +51,12 @@ class Api::UsersController < ApplicationController
     end
   end
 
-
   def destroy
     @user.destroy
   end
 
-
   private
+
   def set_user
     @user = User.find(params[:id])
   end

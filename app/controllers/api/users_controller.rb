987fba615a5a3b8c68381212ebc202e09a4a3ad7
Kevin Peery <kevin.peery@gmail.com>
2019-01-09 15:32:44 -0700

    edit update method in users controller for image uploads

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,30 +2,6 @@ class Api::UsersController < ApplicationController
   before_action :set_user, only: [:show, :delete, :update]
   before_action :authenticate_user!
 
-  def update
-    user = User.find(params[:id])
-    user.name = params[:name] ? params[:name] : user.name
-    user.email = params[:email] ? params[:email] : user.email
-
-    file = params[:file]
-
-    if file
-      begin
-        ext = File.extname(file.tempfile)
-        cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
-        user.image = cloud_image["secure_url"]
-      rescue => e
-        render json: {errors: e}, status: 422
-      end
-    end
-
-    if user.save
-      render json: user
-    else
-      render json: {errors: user.errors.full_messages}, status: 422
-    end
-  end
-
   def index
     render json: User.all
   end
@@ -44,10 +20,25 @@ class Api::UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      render json: @user
+    user = User.find(params[:id])
+    user.email = params[:email] ? params[:email] : user.email
+
+    file = params[:file]
+
+    if file
+      begin
+        ext = File.extname(file.tempfile)
+        cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
+        user.image = cloud_image["secure_url"]
+      rescue => e
+        render json: {errors: e}, status: 422
+      end
+    end
+
+    if user.save
+      render json: user
     else
-      render json: @user.errors, status: 422
+      render json: {errors: user.errors.full_messages}, status: 422
     end
   end
 

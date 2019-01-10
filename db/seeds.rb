372a75f69155b66d65baf49a7cfefe607a5130b9
Jordan Jackson <jordanrjackson0275@gmail.com>
2019-01-09 17:09:06 -0700

    all data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,51 @@
-10.times do
-  application = Application.new(
-    submission_date: Faker::Date.between(2.days.ago, Date.today),
-    notes: Faker::FamousLastWords,
-    title: Faker::Job.title,
-    status: false,
-    company_id: Faker::Company.name,
+20.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    nickname: Faker::Ancient.god,
+    image: Faker::Placeholdit.image,
+    email: Faker::Internet.email,
+    password: "password",
+    cohort: "Winter-18",
+    github: Faker::Internet.url,
+    linkedin: Faker::Internet.free_email,
+    resume: "resume",
+    dob: Faker::Date.birthday(18, 65),
+    college_degree: Faker::Educator.course,
+    employment_status: false,
+    sex: Faker::Gender.binary_type,
+    admin: false,
   )
-  application.save
-
-10.times do
-  company = Company.new(
-    name: Faker::Company.name,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    zip: Faker::Address.zip,
-    website_url: Faker::Internet.domain_name,
-  )
-  company.save
 
   5.times do
-    interview = Interview.new(
+    company = Company.create(
+      name: Faker::Company.name,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip,
+      website_url: Faker::Internet.domain_name,
+      )
+
+    application = Application.create(
+      submission_date: Faker::Date.between(2.days.ago, Date.today),
+      notes: Faker::FamousLastWords.last_words,
+      title: Faker::Job.title,
+      status: false,
+      user_id: user.id,
+      company_id: company.id,
+      )
+    
+    interview = Interview.create(
       date: Faker::Date.forward(23),
       title: Faker::Job.title,
       notes: Faker::DumbAndDumber.quote,
-      application_id: Faker::IDNumber.invalid,
-    )
-    interview.save
+      application_id: application.id,
+      )
 
-  15.times do
-    offer = Offer.new(
+    offer = Offer.create(
       salary: Faker::Commerce.price,
       accepted: false,
-      application_id: Faker::IDNumber.invalid,
-    )
-  offer.save
-
-  20.times do
-    user = User.new(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      nickname: Faker::Ancient.god,
-      image: Faker::Placeholdit.image,
-      email: Faker::Internet.email,
-      cohort: Winter-18,
-      dob: Faker::Date.birthday(18, 65),
-      college_degree: Faker::Educator.course,
-      employment_status: false,
-      sex: Faker::Gender.binary_type,
-      admin: false,
-    )
-    user.save
+      application_id: application.id,
+      )
+  end
 end

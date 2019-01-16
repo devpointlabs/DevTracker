61061ce9b4a9832c5c3220cb664179a152acff0a
MichaelJDaniel <soldbymichaeld@gmail.com>
2019-01-16 15:35:27 -0700

    finish contact redux reducer

--- client/src/reducers/contacts.js
+++ client/src/reducers/contacts.js
@@ -1,25 +1,40 @@
 import axios from "axios"
 
-const ADD_CONTACT  = "ADD_CONTACT";
+const ADD_CONTACT = "ADD_CONTACT";
 const GET_CONTACTS = "GET_CONTACTS";
+const DELETE_CONTACT = "DELETE_CONTACT"
 
 export const getContacts = (user_id) => {
-  return dispatch => {
+  return (dispatch) => {
     axios.get(`/api/users/${user_id}/contacts`)
-    .then(res => {
-      dispatch({ type: GET_CONTACTS, contacts: res.data })
-    })
-    .catch(err => console.log(err));
+      .then(res => {
+        dispatch({ type: GET_CONTACTS, contacts: res.data })
+      })
+      .catch(err => console.log(err));
   };
 };
 
 export const addContact = (contact) => {
-  return dispatch => {
+  return (dispatch) => {
     axios.post(`/api/users/${contact.user_id}/contacts`, { contact })
-    .then(res => dispatch({ type: ADD_CONTACT, contact: res.data[0] }))
-    .catch(err => console.log(err));
+      .then(res => dispatch({ type: ADD_CONTACT, contact: res.data[0] }))
+      .catch(err => console.log(err));
   };
 };
+
+export const deleteContact = (user_id, contact_id) => {
+  return (dispatch) => {
+    axios.delete(`/api/users/${user_id}/contacts/${contact_id}`)
+      .then(() => dispatch({ type: DELETE_CONTACT, user_id, contact_id }))
+  }
+}
+
+export const updateContact = (user, contact) => {
+  return (dispatch) => {
+    axios.update(`/api/users/${user.id}/contacts/${contact.id}`, contact)
+      .then(res => dispatch({ type: UPDATE_CONTACT, contact: res.data }))
+  }
+}
 
 export default (state = [], action) => {
   switch (action.type) {
@@ -29,5 +44,15 @@ export default (state = [], action) => {
       return [action.contact, ...state];
     default:
       return state;
+    case UPDATE_APP:
+      return state.map(c => {
+        if (c.id === action.contact.id)
+          return action.contact
+        return c
+      })
+    case DELETE_APP:
+      return state.filter(c => c.id !== action.id)
+    default:
+      return state;
   }
-};
+};  
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, } from 'redux';
 import companies from './companies';
 import applications from './applications';
+import contacts from './contacts'
 
 const rootReducer = combineReducers({
     companies,
-    applications
+    applications,
+    contacts
 });
 
 export default rootReducer;

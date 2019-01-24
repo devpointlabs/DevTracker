7cd820a7383a6e77c21f9011229b9f75d420ce66
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-23 19:12:35 -0700

    edit dashboard container heights

--- client/src/components/Dashboard/JobApplications.js
+++ client/src/components/Dashboard/JobApplications.js
@@ -275,7 +275,7 @@ const ApplicationsContainer = styled.div`
   border-radius: 5px;
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   position: relative;
-  min-height: 265px;
+  min-height: 300px;
 `;
 
 const SectionTitle = styled.h3`
